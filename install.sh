bf5919f0cc73f8af05603ebfb55b5f4464843928
tajogi <120653132+tajogitajogi@users.noreply.github.com>
2023-02-08 09:42:46 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ dpkg -i *.deb > /dev/null
 rm /etc/squid/squid.conf
 cp squid.conf /etc/squid/squid.conf
 openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -keyout /etc/squid/squid.pem -out /etc/squid/squid.pem
-chown proxy:proxy squid.pem
+chown proxy:proxy /etc/squid/squid.pem
 chmod 640 /etc/squid/squid.pem
 openssl x509 -outform der -in /etc/squid/squid.pem -out /etc/squid/squid.crt
 /usr/lib/squid/security_file_certgen -c -s /var/lib/ssl_db -M 4MB
